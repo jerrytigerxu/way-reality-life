a9fd067747f67a057c0c95ef0257da21bbdb8aa0
jerrytigerxu <jere.tiger.xu@gmailc.om>
2020-06-18 08:39:36 -0400

    header

--- src/components/header.js
+++ src/components/header.js
@@ -14,13 +14,6 @@ const Header = ({ siteTitle }) => (
         </Link>
       </div>
 
-      <div id="navbar-menu" className="navbar-menu is-static">
-        <div className="navbar-end">
-          <Link to="/page-2" className="navbar-item is-secondary">
-            Page 2
-          </Link>
-        </div>
-      </div>
     </div>
   </nav>
 )
